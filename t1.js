eae40123e918eba31f0d9ab4fc51ae9ffe7ff4f7
atomthink <69460866+atomthink@users.noreply.github.com>
2022-04-29 18:52:29 +0530

    Update t1.js

--- t1.js
+++ t1.js
@@ -5,7 +5,7 @@ for (item of buttons) {
     item.addEventListener('click', (e) => {
         buttonText = e.target.innerText;
         console.log('Button text is ', buttonText);
-        if (buttonText == 'X') {
+        if (buttonText == 'x') {
             buttonText = '*';
             screenValue += buttonText;
             screen.value = screenValue;
